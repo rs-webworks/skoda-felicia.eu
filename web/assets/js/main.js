61a1bd477253fb5e6afe69a56ab75f2079e0807b
Raito Akehanareru <raito.akehanareru@gmail.com>
2016-11-14 09:44:44 +0100

    CMS skeleton preparation

---
+++ web/assets/js/main.js
@@ -0,0 +1,55 @@
+$(function () {
+
+    $('.home-header .display-table').animate({opacity: 1}, 2000);
+
+    $(".progress-bar-linear .progress-bar").each(function (index, value) {
+        $(this).animate({
+            width: $(this).attr('aria-valuenow') + '%'
+        }, 2500);
+    });
+
+
+    // Smooth scrolling effect
+    // -----------------------------------------------------------------------------------------------------------------
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    // Fade in on scroll
+    // -----------------------------------------------------------------------------------------------------------------
+    // on load
+    uncoverVisibleObjects();
+
+    // on scroll
+    $(window).scroll(function () {
+        uncoverVisibleObjects();
+    });
+
+    function uncoverVisibleObjects() {
+        $('.hideme').each(function (i) {
+            var bottom_of_object = $(this).offset().top;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_object + 70) {
+                $(this).animate({'opacity': 1}, 1000);
+            }
+        });
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+});
