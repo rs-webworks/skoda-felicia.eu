9592677f3e135a0e048578c5c9730a914b717143
Martin Jagr <mj@2it.cz>
2017-02-28 17:25:45 +0100

    App manifest, ServiceWorker

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -28,6 +28,19 @@ $(function () {
     });
 
 
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('./assets/js/sw.js').then(function (registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(function (err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+
+
     // if ($('.sortable').length > 0) {
     //     Sortable.create($('.sortable')[0], {
     //         'handle': '.dragger',
